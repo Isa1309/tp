91d674ac44a34f404d31ab2d57155c114a757886
maksim4k1 <maksim4k1@gmail.com>
2024-05-11 20:02:27 +0700

    created schemas for animals

---
+++ schemas/animals.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class Animal(BaseModel):
+  id: str
+  name: str
+
+class Breed(BaseModel):
+  id: str
+  name: str
+  animal_id: str
+
+class CreateAnimal(BaseModel):
+  name: str
