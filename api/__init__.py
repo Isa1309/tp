9831138f9aee564f35987189a7149b135c53f894
maksim4k1 <maksim4k1@gmail.com>
2024-05-12 14:46:07 +0700

    created and implemented requests for schedules

--- api/__init__.py
+++ api/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from api.users import router as users_router
 from api.animals import router as animals_router
 from api.pets import router as pets_router
+from api.schedules import router as schedules_router
 
 router = APIRouter()
 
 router.include_router(users_router)
 router.include_router(animals_router)
 router.include_router(pets_router)
+router.include_router(schedules_router)
