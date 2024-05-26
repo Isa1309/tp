8f8d5a4d00d0f456a35e30f88b3d04d6776c7a1e
Sweche21 <Rez1ch2@mail.ru>
2024-05-26 20:33:49 +0700

    create EditMedicalCard model

--- schemas/medical_info.py
+++ schemas/medical_info.py
@@ -2,45 +2,64 @@ from pydantic import BaseModel
 
 from schemas.users import TokenAuth
 
+
 class Vaccination(BaseModel):
-  id: str
-  card_id: str
-  title: str
-  date: int
-  periodicity: int
+    id: str
+    card_id: str
+    title: str
+    date: int
+    periodicity: int
+
 
 class MedicalInfo(BaseModel):
-  id: str
-  card_id: str
-  date: int
-  clinic: str
-  doctor: str
-  symptoms: str
-  diagnosis: str
-  recipe: str
+    id: str
+    card_id: str
+    date: int
+    clinic: str
+    doctor: str
+    symptoms: str
+    diagnosis: str
+    recipe: str
+
 
 class MedicalCard(BaseModel):
-  id: str
-  owner_id: str
-  pet_id: str
-  vaccinations: list[Vaccination]
-  medical_info: list[MedicalInfo]
+    id: str
+    owner_id: str
+    pet_id: str
+    vaccinations: list[Vaccination]
+    medical_info: list[MedicalInfo]
+
 
 class CreateVaccination(BaseModel):
-  title: str
-  date: int
-  periodicity: int
+    title: str
+    date: int
+    periodicity: int
+
 
 class CreateMedicalInfo(BaseModel):
-  date: int
-  clinic: str
-  doctor: str
-  symptoms: str
-  diagnosis: str
-  recipe: str
+    date: int
+    clinic: str
+    doctor: str
+    symptoms: str
+    diagnosis: str
+    recipe: str
+
+
+class EditMedicalCard(BaseModel):
+    owner: TokenAuth
+    pet_id: str
+    vaccinations: list[CreateVaccination]
+    medical_info: list[CreateMedicalInfo]
+    card_id: str
+    date: int
+    clinic: str
+    doctor: str
+    symptoms: str
+    diagnosis: str
+    recipe: str
 
 class CreateMedicalCard(BaseModel):
-  owner: TokenAuth
-  pet_id: str
-  vaccinations: list[CreateVaccination]
-  medical_info: list[CreateMedicalInfo]
+    owner: TokenAuth
+    pet_id: str
+    vaccinations: list[CreateVaccination]
+    medical_info: list[CreateMedicalInfo]
